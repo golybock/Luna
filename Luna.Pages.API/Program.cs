a92d7a32d5ca14911631e36b0fddfdcf5184c537
golybock <teplovartem3094@gmail.com>
2025-10-01 23:50:09 +0300

    Доработка апи сервиса страниц

--- Luna.Pages.API/Program.cs
+++ Luna.Pages.API/Program.cs
@@ -1,12 +1,18 @@
 using Luna.Pages.API.Hubs;
+using Luna.Pages.Repositories.Context;
 using Luna.Pages.Repositories.Repositories.Page.Command;
 using Luna.Pages.Repositories.Repositories.Page.Query;
-using Luna.Pages.Repositories.Repositories.PageVersion;
 using Luna.Pages.Repositories.Repositories.PageVersion.Command;
 using Luna.Pages.Repositories.Repositories.PageVersion.Query;
+using Luna.Pages.Repositories.Repositories.WorkspaceUsers;
+using Luna.Pages.Repositories.WorkspacePermissionRepository;
+using Luna.Pages.Services.PermissionEventHandler;
 using Luna.Pages.Services.Services;
+using Luna.Pages.Services.Services.WorkspacePermissionService;
+using Luna.Tools.Database.Npgsql.Options;
 using Luna.Tools.Exception;
-using MongoDB.Bson;
+using Luna.Tools.SharedModels.Models.Kafka;
+using Microsoft.EntityFrameworkCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -23,7 +29,7 @@ builder.Services.AddCors(options =>
 		policy.AllowAnyHeader()
 			.AllowAnyMethod()
 			.AllowCredentials()
-			.SetIsOriginAllowed(origin => true);;
+			.SetIsOriginAllowed(origin => true);
 	});
 });
 
@@ -31,24 +37,44 @@ builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 
+builder.Services.AddScoped<IPageService, PageService>();
+builder.Services.AddScoped<IWorkspacePermissionService, WorkspacePermissionService>();
+builder.Services.AddScoped<IPermissionEventHandler, PermissionEventHandler>();
 
-// builder.Logging.AddJsonConsole(options =>
-// {
-// 	options.IncludeScopes = true;
-// 	options.TimestampFormat = "yyyy-MM-dd HH:mm:ss ";
-// });
+builder.Services.Configure<KafkaSettings>(builder.Configuration.GetSection("Kafka"));
+builder.Services.AddHostedService<PermissionEventConsumerService>();
 
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<PageService>());
 
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+	options.Configuration = "127.0.0.1:6379";
+	options.InstanceName = "pages:";
+});
+
+builder.Services.AddSingleton<IWorkspacePermissionCacheRepository, WorkspacePermissionCacheRepository>(provider => new WorkspacePermissionCacheRepository(builder.Configuration.GetConnectionString("redis")));
+
+DatabaseOptions databaseOptions = new DatabaseOptions()
+{
+	ConnectionString = builder.Configuration.GetConnectionString("luna_pages") ?? throw new InvalidOperationException()
+};
+
+builder.Services.AddDbContext<LunaPagesContext>(options =>
+	options.UseNpgsql(databaseOptions.ConnectionString));
+
 IConfigurationSection mongoSettings = builder.Configuration.GetSection("MongoDB");
+
+
 string connectionString = mongoSettings.GetValue<string>("ConnectionString") ??
                           throw new InvalidOperationException("MongoDB connection string is required");
+
 string databaseName = mongoSettings.GetValue<string>("DatabaseName") ??
                       throw new InvalidOperationException("MongoDB database name is required");
 string pageCollectionName = mongoSettings.GetValue<string>("PagesCollectionName") ?? "page";
 string pageVersionCollectionName = mongoSettings.GetValue<string>("PageVersionsCollectionName") ?? "page_versions";
 
-// page
+builder.Services.AddScoped<IWorkspaceUserRepository, WorkspaceUserRepository>();
+
 builder.Services.AddScoped<IPageQueryRepository>(provider =>
 {
 	ILogger<PageQueryRepository> logger = provider.GetRequiredService<ILogger<PageQueryRepository>>();
@@ -72,8 +98,6 @@ builder.Services.AddScoped<IPageVersionCommandRepository>(provider =>
 	ILogger<PageVersionCommandRepository> logger = provider.GetRequiredService<ILogger<PageVersionCommandRepository>>();
 	return new PageVersionCommandRepository(connectionString, databaseName, pageVersionCollectionName, logger);
 });
-
-builder.Services.AddScoped<IPageService, PageService>();
 
 WebApplication app = builder.Build();
 
