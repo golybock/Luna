d3bfaa73766d40bc3a1b907bbed90b96ad093486
golybock <teplovartem3094@gmail.com>
2025-10-16 20:37:57 +0300

    Добавлены уведомления в проект

--- luna-client/src/layout/Layout.tsx
+++ luna-client/src/layout/Layout.tsx
@@ -4,7 +4,8 @@ import { PropsWithChildren } from "react";
 import Providers from "@/layout/providers/Providers";
 import DataInitializer from "@/layout/DataInitializer";
 import { ModalProvider } from "@/layout/ModalContext";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Layout({ children }: PropsWithChildren) {
 	return (
@@ -12,6 +13,11 @@ export default function Layout({ children }: PropsWithChildren) {
 			<DataInitializer>
 				<ModalProvider>
 					{children}
+					<ToastContainer
+						position="top-right"
+						theme="colored"
+						style={{ zIndex: "99999" }}
+					/>
 				</ModalProvider>
 			</DataInitializer>
 		</Providers>
