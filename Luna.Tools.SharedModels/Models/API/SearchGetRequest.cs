26a3b3855fced69692e81908c96d140cfafa26fa
golybock <teplovartem3094@gmail.com>
2025-10-16 19:03:18 +0300

    Созданы модели для поиска и настроек поиска

---
+++ Luna.Tools.SharedModels/Models/API/SearchGetRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Luna.Tools.SharedModels.Models.API;
+
+public class SearchGetRequest
+{
+	public Guid UserId { get; set; }
+	public Guid WorkspaceId { get; set; }
+	public string Query { get; set; } = null!;
+	public int From { get; set; } = 0;
+	public int Size { get; set; } = 10;
+}
---
+++ Luna.Tools.SharedModels/Models/Search/ElasticSearchSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Luna.Tools.SharedModels.Models.Search;
+
+public class ElasticSearchSettings
+{
+	public string Url { get; set; } = null!;
+	public string DefaultIndex { get; set; } = null!;
+	public int NumberOfShards { get; set; } = 1;
+	public int NumberOfReplicas { get; set; } = 0;
+}
