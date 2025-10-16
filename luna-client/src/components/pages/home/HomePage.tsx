8c950e3b68b7f0845574bc3deea2b5bda8ceea73
golybock <teplovartem3094@gmail.com>
2025-10-16 19:14:50 +0300

    Добавлен поиск на основной странице рабочего пространства

--- luna-client/src/components/pages/home/HomePage.tsx
+++ luna-client/src/components/pages/home/HomePage.tsx
@@ -1,48 +1,126 @@
 ﻿'use client';
 
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import styles from "./HomePage.module.scss";
 import Card from "@/ui/card/Card";
 import Input from "@/ui/input/Input";
 import Button from "@/ui/button/Button";
 import Image from "next/image";
-import { IPageLightView } from "@/types/page/pageLightView";
-import { pageHttpProvider } from "@/http/pageHttpProvider";
 import { useWorkspaces } from "@/store/hooks/useWorkspaces";
 import { EmojiPicker } from "@/ui/emojiPicker/EmpojiPicker";
+import { useRouter } from "next/navigation";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "@/store/store";
+import { getWorkspacePages } from "@/store/slices/pagesSlice";
+import { usePages } from "@/store/hooks/usePages";
+import { getPageStatistic } from "@/store/slices/workspaceSlice";
+import { pageHttpProvider } from "@/http/pageHttpProvider";
+import { LightPageView } from "@/models/page/view/LightPageView";
 
-export const HomePage : React.FC= () => {
+export const HomePage: React.FC = () => {
 
-	const [pages, setPages] = useState<IPageLightView[]>([]);
-	const { selectedWorkspaceId } = useWorkspaces();
+	const { pages } = usePages();
+	const { selectedWorkspaceId, selectedWorkspacePageStatistic } = useWorkspaces();
+
+	const [searchQuery, setSearchQuery] = useState("");
+	const [searchedPages, setSearchedPages] = useState<LightPageView[]>([]);
+
+	const router = useRouter();
+	const dispatch = useDispatch<AppDispatch>();
 
 	useEffect(() => {
-		const getPages = async () => {
-			if (selectedWorkspaceId) {
-				const pages = await pageHttpProvider.getWorkspacePages(selectedWorkspaceId);
+		if (selectedWorkspaceId) {
+			dispatch(getWorkspacePages(selectedWorkspaceId));
+			dispatch(getPageStatistic(selectedWorkspaceId));
+		}
+	}, [selectedWorkspaceId])
 
-				setPages(pages);
-			}
+	const handleOnPageClick = useCallback((pageId: string) => {
+		router.push(`/${selectedWorkspaceId}/${pageId}`);
+	}, [selectedWorkspaceId]);
+
+	const handleSearchClick = useCallback(async () => {
+		if(selectedWorkspaceId == null) return;
+
+		if(searchQuery == ""){
+			setSearchedPages([]);
+			return;
 		}
 
-		getPages();
-	}, [selectedWorkspaceId])
+		const pages = await pageHttpProvider.searchPages(selectedWorkspaceId, searchQuery);
+		setSearchedPages(pages);
+	}, [searchQuery])
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.content}>
 				<h4>Search anything</h4>
 				<Card className={styles.contentContainer} padding="small">
-					<Input placeholder="search"/>
-					<Button variant="secondary" className={styles.searchButton}>
-						<Image src="/icons/search_24.svg" alt="search" width={24} height={24} />
+					<Input
+						placeholder="search"
+						value={searchQuery}
+						onChange={(e) => setSearchQuery(e.target.value)}
+					/>
+					<Button
+						variant="secondary"
+						className={styles.searchButton}
+						onClick={handleSearchClick}
+					>
+						<Image
+							src="/icons/search_24.svg"
+							alt="search"
+							width={24}
+							height={24}
+						/>
 					</Button>
 				</Card>
 				<div className={styles.contentContainer}>
-					<p>Workspace pages</p>
+					{searchedPages.length > 0 && (
+						<>
+							<p>Search result</p>
+							<div className={styles.pagesCarousel}>
+								{searchedPages.map((page) => (
+									<Card
+										padding="none"
+										key={page.id}
+										className={styles.pageCard}
+										onClick={() => handleOnPageClick(page.id)}
+									>
+										{page.cover && (
+											<Image
+												src={page.cover}
+												alt="cover"
+												width={150}
+												height={75}
+											/>
+										)}
+										<div className={styles.pageCardContent}>
+											{page.emoji && (
+												<EmojiPicker
+													value={page.emoji}
+													disabled={true}
+													className={styles.pageEmojiPicker}
+												/>
+											)}
+											<h4>{page.title}</h4>
+										</div>
+									</Card>
+								))}
+							</div>
+						</>
+					)}
+					<p>Recent pages</p>
 					<div className={styles.pagesCarousel}>
 						{pages.map((page) => (
-							<Card padding="small" key={page.id} className={styles.pageCard}>
+							<Card
+								padding="none"
+								key={page.id}
+								className={styles.pageCard}
+								onClick={() => handleOnPageClick(page.id)}
+							>
+								{page.cover && (
+									<Image src={page.cover} alt={"cover"} width={150} height={75}/>
+								)}
 								<div className={styles.pageCardContent}>
 									{page.emoji && (
 										<EmojiPicker
@@ -57,6 +135,20 @@ export const HomePage : React.FC= () => {
 						))}
 					</div>
 				</div>
+				{selectedWorkspacePageStatistic && (
+					<Card>
+						<div>
+							<h5>Statistic</h5>
+							<div className="col">
+								<p>Total pages: {selectedWorkspacePageStatistic.totalPages}</p>
+								<p>Archived pages: {selectedWorkspacePageStatistic.archivedPages}</p>
+								<p>Pages in trash: {selectedWorkspacePageStatistic.deletedPages}</p>
+								<p>Pinned pages: {selectedWorkspacePageStatistic.pinnedPages}</p>
+								<p>Templates: {selectedWorkspacePageStatistic.templates}</p>
+							</div>
+						</div>
+					</Card>
+				)}
 			</div>
 		</div>
 	)
