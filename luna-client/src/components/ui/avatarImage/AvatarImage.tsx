7c34ca19d37a55cb145f347278c05788ac8f7bac
golybock <teplovartem3094@gmail.com>
2025-10-21 21:35:15 +0300

    Вынесены компоненты аватарки пользователя и загрузки

---
+++ luna-client/src/components/ui/avatarImage/AvatarImage.tsx
@@ -0,0 +1,26 @@
+﻿import React from "react";
+import styles from "./AvatarImage.module.scss";
+
+interface AvatarImageProps {
+	src: string;
+	alt: string;
+	initials?: string;
+}
+
+export const AvatarImage: React.FC<AvatarImageProps> = ({src, alt, initials}) => {
+	return (
+		<div className={styles.avatar}>
+			{src ? (
+				<img
+					src={src}
+					alt={alt}
+					className={styles.avatarImage}
+				/>
+			) : (
+				<div className={styles.avatarPlaceholder}>
+					{initials}
+				</div>
+			)}
+		</div>
+	)
+}
---
+++ luna-client/src/components/ui/loading/Loading.tsx
@@ -0,0 +1,12 @@
+﻿import styles from "./Loading.module.scss";
+
+export const Loading = () => {
+	return (
+		<div className={styles.container}>
+			<div className={styles.loading}>
+				{/*<loader>*/}
+				<h2>Loading...</h2>
+			</div>
+		</div>
+	)
+}
