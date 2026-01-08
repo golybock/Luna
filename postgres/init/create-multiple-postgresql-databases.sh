540e9deeb8d7c08da78078ca62a293db3c2282ef
golybock <teplovartem3094@gmail.com>
2026-01-08 19:39:58 +0300

    Исправление скрипта создания бд при запуске docker compose

--- postgres/init/create-multiple-postgresql-databases.sh
+++ postgres/init/create-multiple-postgresql-databases.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-
 set -e
-set -u
 
-function create_user_and_database() {
-    local database=$1
-    echo "Creating database '$database'"
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-        CREATE DATABASE $database;
-        GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
+create_db() {
+  local db="$1"
+  echo "Creating database $db"
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<EOSQL
+CREATE DATABASE "$db";
+GRANT ALL PRIVILEGES ON DATABASE "$db" TO "$POSTGRES_USER";
 EOSQL
 }
 
-if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
-    echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
-    for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
-        create_user_and_database $db
-    done
-    echo "Multiple databases created"
+if [ -n "${POSTGRES_MULTIPLE_DATABASES:-}" ]; then
+  echo "Databases: $POSTGRES_MULTIPLE_DATABASES"
+  for db in $(echo "$POSTGRES_MULTIPLE_DATABASES" | tr ',' ' '); do
+    create_db "$db"
+  done
 fi
