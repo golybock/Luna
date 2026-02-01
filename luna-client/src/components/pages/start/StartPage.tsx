1ac5f718d1d35199c3d7038651fb261638a87cef
golybock <teplovartem3094@gmail.com>
2026-02-01 21:45:47 +0300

    Обновление стилей страницы выбора рабочего пространства

--- luna-client/src/components/pages/start/StartPage.tsx
+++ luna-client/src/components/pages/start/StartPage.tsx
@@ -11,7 +11,6 @@ import { AppDispatch } from "@/store/store";
 import { useDispatch } from "react-redux";
 import { useModal } from "@/layout/ModalContext";
 import { CreateWorkspaceModal } from "@/components/modals/createWorkspace/CreateWorkspaceModal";
-import { getAvailableWorkspaces } from "@/store/slices/workspaceSlice";
 
 export const StartPage: React.FC = () => {
 
@@ -26,9 +25,7 @@ export const StartPage: React.FC = () => {
 		if (selectedWorkspaceId != null) {
 			router.push(`${selectedWorkspaceId}`);
 		}
-
-		dispatch(getAvailableWorkspaces());
-	}, [selectedWorkspaceId]);
+	}, [selectedWorkspaceId, router, dispatch]);
 
 	const selectWorkspace = (selectedWorkspaceId: string) => {
 		setSelectedWorkspace(selectedWorkspaceId);
@@ -41,10 +38,16 @@ export const StartPage: React.FC = () => {
 	return (
 		<div className={styles.container}>
 			<div className={styles.content}>
-				<h3>Select workspace</h3>
+				<div className={styles.header}>
+					<h3>Select workspace</h3>
+					<p>Choose where you want to work or create a new workspace.</p>
+				</div>
 				<div className={styles.cards}>
-					<Card className={styles.workspaceCard} onClick={handleCreateWorkspace}>
-						<Plus color="gray"/>
+					<Card className={styles.createCard} onClick={handleCreateWorkspace}>
+						<div className={styles.createIcon}>
+							<Plus size={18} />
+						</div>
+						<p>Create workspace</p>
 					</Card>
 					{workspaces.map((workspace) => (
 						<Card
@@ -54,8 +57,8 @@ export const StartPage: React.FC = () => {
 							onClick={() => selectWorkspace(workspace.id)}
 						>
 							<div className={styles.workspaceName}>
-								<h5>{workspace.name}</h5>
-								<p>Users: {workspace.users.length}</p>
+								<h5 title={workspace.name}>{workspace.name}</h5>
+								<p>{workspace.users.length} members</p>
 							</div>
 						</Card>
 					))}
