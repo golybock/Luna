6f0a13e1495d9094458e5114bfca8f7032c43d5e
golybock <teplovartem3094@gmail.com>
2026-02-01 21:31:47 +0300

    В скрипты бд добавлены таблицы outbox

--- postgres/init/luna_auth.sql
+++ postgres/init/luna_auth.sql
@@ -34,7 +34,23 @@ create table session_archive
 
 COMMENT ON TABLE "session_archive" IS 'Сессия, хранится в redis и в архив таблице в postgresql';
 
+create table outbox_messages
+(
+    id           uuid        not null primary key,
+    type         text        not null,
+    payload      jsonb       not null,
+    status       int         not null,
+    attempts     int         not null default 0,
+    created_at   timestamptz not null default now(),
+    processed_at timestamptz,
+    last_error   text,
+    locked_until timestamptz
+);
+
+COMMENT ON TABLE outbox_messages IS 'Outbox сообщения для RabbitMQ';
+
 CREATE INDEX idx_user_email ON auth_users (email);
+CREATE INDEX idx_outbox_status_locked ON outbox_messages (status, locked_until, created_at);
 
 CREATE OR REPLACE FUNCTION update_timestamp()
     RETURNS TRIGGER AS
--- postgres/init/luna_pages.sql
+++ postgres/init/luna_pages.sql
@@ -11,8 +11,24 @@ create table workspace_users
     UNIQUE ("user_id", "workspace_id")
 );
 
+create table outbox_messages
+(
+    id           uuid        not null primary key,
+    type         text        not null,
+    payload      jsonb       not null,
+    status       int         not null,
+    attempts     int         not null default 0,
+    created_at   timestamptz not null default now(),
+    processed_at timestamptz,
+    last_error   text,
+    locked_until timestamptz
+);
+
+COMMENT ON TABLE outbox_messages IS 'Outbox сообщения для Search индексации';
+
 CREATE INDEX idx_workspace_users_workspace ON workspace_users(workspace_id);
 CREATE INDEX idx_workspace_users_user ON workspace_users(user_id);
+CREATE INDEX idx_outbox_status_locked ON outbox_messages (status, locked_until, created_at);
 
 CREATE OR REPLACE FUNCTION update_timestamp()
     RETURNS TRIGGER AS $$
