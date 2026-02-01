e76d73fd868a5894d10cf629f681c9cd46fe4754
golybock <teplovartem3094@gmail.com>
2026-02-01 21:23:21 +0300

    Исправление скрипта создания индекса для поиска по страницам

--- elasticSearch/init/init-pages.sh
+++ elasticSearch/init/init-pages.sh
@@ -2,33 +2,32 @@
     echo "Waiting for Elasticsearch to be ready..."
     sleep 10
 
-    echo "Creating 'pages' index..."
-    curl -X PUT "http://elasticsearch:9200/pages" -H 'Content-Type: application/json' -d'
-    {
-      "settings": {
-        "number_of_shards": 1,
-        "number_of_replicas": 0
-      },
-      {
-        "mappings": {
-          "properties": {
-            "blocks": {
-              "type": "nested",
-              "properties": {
-                "blockId": { "type": "keyword" },
-                "pageId": { "type": "keyword" },
-                "type": { "type": "keyword" },
-                "content": { "type": "text" }
-              }
-            },
-            "pageId": { "type": "keyword" },
-            "title": { "type": "text" },
-            "description": { "type": "text" },
-            "workspaceId": { "type": "keyword" },
-            "updatedAt": { "type": "date" }
-          }
+echo "Creating 'pages' index..."
+curl -X PUT "http://elasticsearch:9200/pages" -H 'Content-Type: application/json' -d'
+{
+  "settings": {
+    "number_of_shards": 1,
+    "number_of_replicas": 0
+  },
+  "mappings": {
+    "properties": {
+      "pageId": { "type": "keyword" },
+      "workspaceId": { "type": "keyword" },
+      "title": { "type": "text", "analyzer": "standard" },
+      "description": { "type": "text", "analyzer": "standard" },
+      "content": { "type": "text", "analyzer": "standard" },
+      "updatedAt": { "type": "date" },
+      "blocks": {
+        "type": "nested",
+        "properties": {
+          "blockId": { "type": "keyword" },
+          "pageId": { "type": "keyword" },
+          "type": { "type": "keyword" },
+          "content": { "type": "text", "analyzer": "standard" }
         }
       }
-    }'
+    }
+  }
+}'
 
     echo "Indexes initialized."
